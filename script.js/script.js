d93d43f806c0597d6cc3e0d191f83fb14bdff0a9
gaolbowo-byte <gaolbowo@gmail.com>
2025-10-21 00:23:27 -0600

    Create script.js

---
+++ script.js/script.js
@@ -0,0 +1,7 @@
+// 1.pilih tombol berdasarkan ID-nya
+const halo rek = documen.getElementById('sapaButton');
+//2.tambahkan'event listener' untuk aksi 'clik'
+halo rek.addEventlistener('clik', function() {
+  // 3. jalankan kode ini ketika tombol di-klik
+    alert('halo! Terima kasih sudah berkunjung!');
+});
